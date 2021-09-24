70d1e867fca25629125f5fdbe1cddebb15afc050
jovanamatk <mat.jovana@yahoo.com>
2021-09-24 15:59:10 +0200

    chore: env variables, error handling

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,10 +1,14 @@
 import Head from "next/head";
+import Link from "next/link";
 import Title from "../components/Title";
 import { getProducts } from "../lib/products";
 
 export async function getStaticProps() {
   const products = await getProducts();
-  return { props: { products }, revalidate: 30 };
+  return {
+    props: { products },
+    revalidate: parseInt(process.env.REVALIDATE_SECONDS),
+  };
 }
 
 function HomePage({ products }) {
@@ -17,7 +21,11 @@ function HomePage({ products }) {
         <Title>Next Shop</Title>
         <ul>
           {products.map((product) => (
-            <li key={product.id}>{product.title}</li>
+            <li key={product.id}>
+              <Link href={`/products/${product.id}`}>
+                <a>{product.title}</a>
+              </Link>
+            </li>
           ))}
         </ul>
       </main>
