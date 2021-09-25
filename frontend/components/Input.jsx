19ae037bbf1b8c7acb6c91fba431bebc3d3c2653
jovanamatk <mat.jovana@yahoo.com>
2021-09-25 18:33:46 +0200

    feat: save jwt as a cookie

--- frontend/components/Input.jsx
+++ frontend/components/Input.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-function Input({ type }) {
-  return <input type={type} className="border rounded px-3 py-1 w-80" />;
+function Input({ type, value, onChange, required }) {
+  return (
+    <input
+      type={type}
+      value={value}
+      onChange={onChange}
+      required={required}
+      className="border rounded px-3 py-1 w-80"
+    />
+  );
 }
 
 export default Input;
--- frontend/pages/sign-in.jsx
+++ frontend/pages/sign-in.jsx
@@ -1,20 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../components/Input";
 import Field from "../components/Field";
 import Page from "../components/Page";
 import Button from "../components/Button";
+import { fetchJson } from "../lib/api";
 
 function SignInPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [status, setStatus] = useState({ loading: false, error: false });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setStatus({ loading: true, error: false });
+    try {
+      const response = await fetchJson("/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      setStatus({ loading: false, error: false });
+      console.log(response);
+    } catch (err) {
+      setStatus({ loading: false, error: true });
+    }
+  };
+
   return (
     <Page title="Sign in">
-      <form>
+      <form onSubmit={handleSubmit}>
         <Field label="Email">
-          <Input type="email" />
+          <Input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
         </Field>
         <Field label="Password">
-          <Input type="password" />
+          <Input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
         </Field>
-        <Button type="submit">Sign in</Button>
+        {status.error && <p className="text-red-700">Invalid credentials</p>}
+        {status.loading ? (
+          <p>Loading...</p>
+        ) : (
+          <Button type="submit">Sign in</Button>
+        )}
       </form>
     </Page>
   );
